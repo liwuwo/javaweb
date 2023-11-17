650b864614de7794d7ca2bb5519f3e79b22acc8d
liwuwo <359199184@qq.com>
2023-11-17 18:57:19 +0800

    update

--- src/main/java/com/algorithm/Result.java
+++ src/main/java/com/algorithm/Result.java
@@ -10,56 +10,8 @@ public class Result {
         int tNodes = 3;
         List<Integer> tFrom = Arrays.asList(1, 1);
         List<Integer> tTo = Arrays.asList(2, 3);
-        System.out.println(getMinCost(val, tNodes, tFrom, tTo));
+
     }
 
-    public static int getMinCost(List<Integer> val, int tNodes, List<Integer> tFrom, List<Integer> tTo) {
-        val.replaceAll(integer -> integer % 2);
 
-        List<Set<Integer>> adjList = new ArrayList<>(tNodes);
-        for (int i = 0; i < tNodes; i++) {
-            adjList.add(new HashSet<>());
-        }
-        for (int i = 0; i < tFrom.size(); i++) {
-            int from = tFrom.get(i) - 1;
-            int to = tTo.get(i) - 1;
-            adjList.get(from).add(to);
-            adjList.get(to).add(from);
-        }
-
-        List<Integer> leaves = new ArrayList<>();
-        for (int i = 0; i < tNodes; i++) {
-            if (adjList.get(i).size() == 1) {
-                leaves.add(i);
-            }
-        }
-
-        int remaining = tNodes;
-        int cost = 0;
-        while (!leaves.isEmpty() && remaining > 2) {
-            remaining -= leaves.size();
-            List<Integer> newLeaves = new ArrayList<>();
-            for (int leaf : leaves) {
-                int parent = adjList.get(leaf).iterator().next();
-                adjList.get(leaf).clear();
-                adjList.get(parent).remove(leaf);
-
-                if (val.get(leaf) == 1) {
-                    cost++;
-                    val.set(parent, 1 - val.get(parent));
-                }
-
-                if (adjList.get(parent).size() == 1) {
-                    newLeaves.add(parent);
-                }
-            }
-            leaves = newLeaves;
-        }
-
-        if (leaves.size() == 2 && val.get(leaves.get(0)) == 1) {
-            cost++;
-        }
-
-        return cost;
-    }
 }
