d1d5a4b769decffc6ce3e89ae0078ef9ab770b67
liwuwo <359199184@qq.com>
2023-11-17 18:59:26 +0800

    update

--- src/main/java/com/algorithm/Result.java
+++ src/main/java/com/algorithm/Result.java
@@ -6,11 +6,11 @@ public class Result {
 
 
     public static void main(String[] args) {
+
         List<Integer> val = Arrays.asList(3, 1, 2);
         int tNodes = 3;
         List<Integer> tFrom = Arrays.asList(1, 1);
         List<Integer> tTo = Arrays.asList(2, 3);
-
     }
 
 
